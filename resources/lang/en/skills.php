894f43697c8aafcdff5e081282b03bf1de2c6f80
Giovanni Bertotti <giovanni.bertottisonaglio@qorvo.com>
2026-03-26 11:48:57 +0100

    Dynamic fill of skills

--- resources/lang/en/skills.php
+++ resources/lang/en/skills.php
@@ -4,38 +4,37 @@
 
 return [
     'title' => 'SKILLS & COMPETENCIES',
-    'soft' => [
-        'title' => 'Soft Skills',
-        'skill1' => 'Teamwork',
-        'skill2' => 'Leadership',
-        'skill3' => 'Self Management',
-        'skill4' => 'Self Taught',
-        'skill5' => 'Quick Grasping',
-        'skill6' => 'Communication',
-        'skill7' => 'Adaptability'
-    ],
-    'hard' => [
-        'title' => 'Hard Skills',
-        'skill1' => 'C & C++',
-        'skill2' => 'PHP ',
-        'skill3' => 'Process Automation ',
-        'skill4' => 'Javascript ',
-        'skill5' => 'Git ',
-        'skill6' => 'Python',
-        'skill7' => 'MySQL ',
-        'skill8' => 'Linux ',
-        'skill9' => 'Web Development ',
-        'skill10' => 'Embedded Software ',
-        'skill11' => 'Circuit Design & Manufacturing'
-    ],
-    'tools' => [
-        'title' => 'Tools',
-        'tool1' => 'Matlab',
-        'tool2' => 'Altium',
-        'tool3' => 'VSCode',
-        'tool4' => 'Proteus',
-        'tool5' => 'PSIM',
-        'tool6' => 'AutoCAD',
-        'tool7' => 'Quartus'
+    'skillset' => [
+        'Soft Skills' => [
+            'Teamwork',
+            'Leadership',
+            'Self Management',
+            'Self Taught',
+            'Quick Grasping',
+            'Communication',
+            'Adaptability'
+        ],
+        'Hard Skills' => [
+            'C & C++',
+            'PHP ',
+            'Process Automation ',
+            'Javascript ',
+            'Git ',
+            'Python',
+            'MySQL ',
+            'Linux ',
+            'Web Development ',
+            'Embedded Software ',
+            'Circuit Design & Manufacturing'
+        ],
+        'Tools' => [
+            'Matlab',
+            'Altium',
+            'VSCode',
+            'Proteus',
+            'PSIM',
+            'AutoCAD',
+            'Quartus'
+        ]
     ]
 ];
--- resources/views/skills.blade.php
+++ resources/views/skills.blade.php
@@ -1,37 +1,9 @@
 <div class="title"> {{ __('skills.title') }} </div>
-<div class="skillset">
-    <div class="subtitle"> {{ __('skills.soft.title') }} </div>
-    <div class="skill"> {{ __('skills.soft.skill1') }} </div>
-    <div class="skill"> {{ __('skills.soft.skill2') }} </div>
-    <div class="skill"> {{ __('skills.soft.skill3') }} </div>
-    <div class="skill"> {{ __('skills.soft.skill4') }} </div>
-    <div class="skill"> {{ __('skills.soft.skill5') }} </div>
-    <div class="skill"> {{ __('skills.soft.skill6') }} </div>
-    <div class="skill"> {{ __('skills.soft.skill7') }} </div>
-    <div class="skill"> {{ __('skills.soft.skill8') }} </div>
-</div>
-
-<div class="skillset">
-    <div class="subtitle"> {{ __('skills.hard.title') }} </div>
-    <div class="skill"> {{ __('skills.hard.skill1') }} </div>
-    <div class="skill"> {{ __('skills.hard.skill2') }} </div>
-    <div class="skill"> {{ __('skills.hard.skill3') }} </div>
-    <div class="skill"> {{ __('skills.hard.skill4') }} </div>
-    <div class="skill"> {{ __('skills.hard.skill5') }} </div>
-    <div class="skill"> {{ __('skills.hard.skill6') }} </div>
-    <div class="skill"> {{ __('skills.hard.skill7') }} </div>
-    <div class="skill"> {{ __('skills.hard.skill8') }} </div>
-    <div class="skill"> {{ __('skills.hard.skill9') }} </div>
-    <div class="skill"> {{ __('skills.hard.skill10') }} </div>
-    <div class="skill"> {{ __('skills.hard.skill11') }} </div>
-</div>
-
-<div class="skillset">
-    <div class="subtitle"> {{ __('skills.tools.title') }} </div>
-    <div class="skill"> {{ __('skills.tools.tool1') }} </div>
-    <div class="skill"> {{ __('skills.tools.tool2') }} </div>
-    <div class="skill"> {{ __('skills.tools.tool3') }} </div>
-    <div class="skill"> {{ __('skills.tools.tool4') }} </div>
-    <div class="skill"> {{ __('skills.tools.tool5') }} </div>
-    <div class="skill"> {{ __('skills.tools.tool6') }} </div>
-</div>
+@foreach (\Lang::get('skills.skillset') as $subtitle => $skillset)
+    <div class="skillset">
+        <div class="subtitle"> {{ $subtitle }} </div>
+        @foreach ($skillset as $skill)
+            <div class="skill"> {{ $skill }} </div>
+        @endforeach
+    </div>
+@endforeach
