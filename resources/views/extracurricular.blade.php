f3d9b1746bbbef78ec080e243eb8595d5acfc9cd
Giovanni Bertotti <giovanni.bertottisonaglio@qorvo.com>
2026-03-26 12:10:57 +0100

    Dynamic fill of extracurricular activities

--- resources/views/extracurricular.blade.php
+++ resources/views/extracurricular.blade.php
@@ -1,39 +1,16 @@
 <div class="title"> {{ __('extracurricular.title') }} </div>
-{{-- <div class="course">
-    <div class="degree">
-        {{ __('extracurricular.activity1.name') }}
-    </div>
-    <div class="info">
-        <div class="left"> {{ __('extracurricular.activity1.info.date') }} </div>
-        <div class="right"> {{ __('extracurricular.activity1.info.location') }} </div>
-    </div>
-    <div class="activities">
-        <i class="fa-solid fa-square"></i> {{ __('extracurricular.activity1.description') }}
-    </div>
-</div> --}}
 
-<div class="course">
-    <div class="degree">
-        {{ __('extracurricular.activity2.name') }}
+@foreach (\Lang::get('extracurricular.activities') as $activity)
+    <div class="activity">
+        <div class="title">
+            {{ $activity['name'] }}
+        </div>
+        <div class="info">
+            <div class="left"> {{ $activity['info']['date'] }} </div>
+            <div class="right"> {{ $activity['info']['location'] }} </div>
+        </div>
+        <div class="description">
+            <i class="fa-solid fa-square"></i> {{ $activity['description'] }}
+        </div>
     </div>
-    <div class="info">
-        <div class="left"> {{ __('extracurricular.activity2.info.date') }} </div>
-        <div class="right"> {{ __('extracurricular.activity2.info.location') }} </div>
-    </div>
-    <div class="activities">
-        <i class="fa-solid fa-square"></i> {{ __('extracurricular.activity2.description') }}
-    </div>
-</div>
-
-<div class="course">
-    <div class="degree">
-        {{ __('extracurricular.activity3.name') }}
-    </div>
-    <div class="info">
-        <div class="left"> {{ __('extracurricular.activity3.info.date') }} </div>
-        <div class="right"> {{ __('extracurricular.activity3.info.location') }} </div>
-    </div>
-    <div class="activities">
-        <i class="fa-solid fa-square"></i> {{ __('extracurricular.activity3.description') }}
-    </div>
-</div>
+@endforeach
